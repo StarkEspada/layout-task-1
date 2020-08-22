cbba044d516d4804a218cc0b30bbaafd697bfb88
Stark <59829975+StarkEspada@users.noreply.github.com>
2020-08-22 16:26:02 +0300

    Add files via upload

---
+++ Cliqto Media/cdn/js/geo/al_ko.js
@@ -0,0 +1,36 @@
+﻿
+(function() {
+    'use strict';
+
+    window.countryList = {
+        al: {
+            name: 'Shqipëria',
+            code: 'al',
+            price: 4990,
+            oldPrice: 9980,
+            labelPrice: 'ALL',
+            phoneHelper: 'Shembull +355 1234567890',
+            nameHelper: 'Florjan Fejzollari',
+            nameError: 'Emri është i nevojshëm',
+            phoneError: 'Numri i telefonit është e nevojshme',
+            countryError: 'Vendi është i nevojshëm',
+            isDefault: true
+        },
+        
+        
+      xk: {
+            name: 'Kosovo',
+            code: 'xk',
+            price: 39,
+            oldPrice: 78,
+            labelPrice: 'EUR',
+            phoneHelper: 'На пример, +383 123456789',
+            nameHelper: 'Boris Paunovic',
+            nameError: 'Име је обавезно',
+            phoneError: 'Погрешно је наведено број',
+            countryError: 'Држава је обавезна',
+        }
+        
+    };
+    
+})();
---
+++ Cliqto Media/js/translater.js
@@ -0,0 +1,174 @@
+var new_lang = {
+
+"lt1": "Ktheni shikimin tuaj <span> </span> <i> <mark> me vetëm një kurs </mark> </i>",
+ 
+"lt2": "<span> Eleminon tensionin </span> dhe dhimbjet e syve brenda vetëm 5 minutave",
+ 
+"lt3": "<span> Riktheni shikimin </span> me 99% për 1 kurs përdorimi të rregullt",
+ 
+"lt4": "<span> Parandalon zhvillimin e sëmundjeve të syrit dhe shikimin e dëmtuar",
+ 
+"lt5": "<b> Vepron mbi </b> <span> shkakun </span> e shikimit të dobët",
+ 
+"lt6": "<b> Mbron </b> <span> gjatë ngarkesave </span>të zgjatura",
+ 
+"lt7": " <b>Eleminon</b>tensionin ",
+ 
+"lt8": "Porositeni <span> me zbritje </span> <span> <mark> në këto momente </mark> </span>",
+ 
+"lt11": "Deri në fund të promovimit kanë mbetur:",
+ 
+"lt12": "<div class = \" form__count-item \"> <span> 0 </span> <span> ore </span> </div>: <div class = \"form__count-item \"> <span> 11 </span> <span> minuta </span> </div>: <div class = \"form__count-item \"> <span> 56 </span> <span> sekonda </span> < / div> ",
+ 
+"lt13": "Shteti",
+ 
+"lt14": "Emri juaj",
+ 
+"lt15": "Numri juaj I telefonit",
+ 
+"lt16": "Blini me zbritje",
+ 
+"lt17": "Përse shikimi im <mark> po përkeqësohet? </mark>",
+ 
+"lt18": "Lentet dhe muskujt e syrit janë përgjegjës për mprehtësinë vizuale. Kur muskujt dobësohen dhe elasticiteti i lenteve zvogëlohet, shikimi fillon të përkeqësohet me shpejtësi.",
+ 
+"lt19": "Kjo në fakt ndodh për disa arsye:",
+ 
+"lt20": "Ndryshimet e moshës",
+ 
+"lt21": "Proceset inflamatore dhe sëmundjet e syrit",
+ 
+"lt22": "Rritje e presionit mbi qafën e syrit",
+ 
+"lt23": "Tendosje e rritur në sy",
+ 
+"lt24": "Furnizimi i pamjaftueshëm me gjak",
+ 
+"lt25": "<mark> Në mungesë të trajtimit, shikimi gradualisht përkeqësohet deri në verbëri të plotë </mark>. Syzet dhe lentet e kontaktit vetëm sa e përkeqësojnë situatën në këtë rast: muskujt e syve ndalojnë plotësisht punën, kjo bën që shikimi të ulet edhe më shpejt.",
+ 
+"lt26": "Aparatet muskulore përqendrojnë sytë",
+ 
+"lt27": "në mënyrë që imazhet e përfituara, duke kaluar nëpër thjerrëza, të projektohen në retinën e syrit ",
+ 
+"lt28": "Çrregullimet e ekuilibrit të muskujve",
+ 
+"lt29": "krijojnë shtypje të pamjaftueshme ose të tepërt të zverkut të syrit",
+ 
+"lt30": "Lentja e syrit deformohet",
+ 
+"lt31": "fokusi shtrembërohet, imazhi turbullohet",
+ 
+"lt32": "Shikimi juaj tashmë <span> mund të ruhet! </span>",
+ 
+"lt33": "Shkencëtarët nga Qendra Oftalmologjike Evropiane kaluan 8 vjet për të zhvilluar medikamentin XX. Zhvillimi kaloi testimin shumë-fazor, u përfitua një licencë dhe certifikatë e cilësisë, si dhe një mori çmimesh për zhvillimin më të mirë në fushën e okulistologjisë",
+ 
+"lt34": "Veçantia e këtij produkti të ri është efekti më i shpejtë, i dukshëm pas 1 kursi të rregullt të përdorimit.",
+ 
+"lt35": "Rekomanduar për përdorim <mark> nga Shoqata e Oftalmologjisë së BE-së </mark>",
+ 
+"lt36": "Përbërja 100% <mark> natyrale </mark>",
+ 
+"lt37": "Efikasiteti është <mark> vërtetuar klinikisht </mark>",
+ 
+"lt39": "<mark> Zhvillimi inovativ </mark> për të rivendosur shikimin njëherë e përgjithmonë",
+ 
+"lt40": "XX është një kompleks biogjenik i ekstrakteve natyrore që kanë një efekt të synuar në përmirësimin e shikimit kur ai fillon të prishet dhe sigurimin e lehtësimit të plotë nga sëmundjet më të zakonshme të syrit njerëzor.",
+ 
+"lt41": "Ky është medikamenti i vetëm që rregullon muskujt e kokërdhokut të syrit, i cili ndihmon për të rivendosur fokusin normal dhe për të rikthyer shikimin e qartë, të ndritshëm, tre-dimensional.",
+ 
+"lt42": "XX përmban <span> <mark> përbërës me cilësi të lartë </mark> </span> me efektivitet të provuar tashmë",
+ 
+"lt43": "EKSTRAKTI I BORONICËS DHE KEMASHEN ME QIME (hawkweed)",
+ 
+"lt44": "Përmirëson mprehtësinë vizuale, forcon retinën",
+ 
+"lt45": "RRUSHI I DETIT (Coccoloba uvifera) DHE RAUWOLFIA",
+ 
+"lt46": "Mbron retinën nga dëmtimi. Përmirëson mprehtësinë vizuale",
+ 
+"lt47": "EKSTRAKTI GINKGO BILOBA",
+ 
+"lt48": "Përmirëson mikroqarkullimin Parandalon brishtësinë kapilare. Përmirëson rrjedhën e gjakut në enët e retinës",
+ 
+"lt49": "VAJI I PESHKAQENIT",
+ 
+"lt50": "Përmirëson elasticitetin e enëve të gjakut, mprehtësinë vizuale dhe kontrastin e perceptimit të imazhit. Normalizon presionin intraokular, siguron parandalimin e glukukomës",
+ 
+"lt51": "SHIKSHA",
+ 
+"lt52": "Parandalon rënien e shikimit të lidhura me moshën. Zvogëlon rrezikun e zhvillimit të degjenerimit makular. Ndihmon në parandalimin e kataraktave",
+ 
+"lt53": "VITAMINS B, C, E",
+ 
+"lt54": "Përmirëson gjendjen e organeve të shikimit. Merr pjesë në proceset e përshtatjes me imazhet e errëta, zvogëlon lodhjen e syve. Përshpejton rigjenerimin e pigmenteve vizuale. Ndihmon në parandalimin e katarakteve",
+ 
+"lt55": "Mendimi i një specialisti <mark> </mark>",
+ 
+"lt56": "Gjatë viteve të kaluara, numri i personave që vuajnë nga humbja e shikimit është rritur 18 herë. Shumë nga pacientët e mi panikojnë dhe më kërkojnë të përshkruaj lente dhe syze për ta, edhe pse nuk e kuptojnë se një masë e tillë do të përkeqësonte më tej situatën, duke i prishur plotësisht sytë. ",
+ 
+"lt57": "Sot egziston një mundësi për të zgjidhur problemin plotësisht. Për këtë qëllim, është zhvilluar XX. Unë ua përshkruaj secilit prej pacientëve të mi dhe në 99% të rasteve ata arrijnë të rikthejnë shikimin e qartë dhe të ndritshëm pas vetëm 1 kursi përdorimi.",
+ 
+"lt58": "Përgatitja përmban përbërës të zgjedhur posaçërisht që rivendosin funksionimin korrekt të muskujve të syrit dhe përmirësojnë gjendjen e thjerrëzave - faktorët kryesorë të një shikimi të mirë.",
+ 
+"lt59": "<mark> Fatmir Agolli </mark>, MD, profesor, okulisti <span> <span> me 20 vjet përvojë pune",
+ 
+"lt60": "Shqyrtime",
+ 
+"lt61": "Krisela HOXHA,",
+ 
+"lt62": "35 vjeç",
+ 
+"lt63": "Kam zbuluar rreth XX isha në poliklinikë, ata po e diskutonin atë atje në mënyrë aktive, kështu që vendosa ta porosisja dhe ta provoja, po sikur të funksiononte me të vërtetë? Mosha tashmë më ka prekur edhe mua, shpesh më duhej të përdorja syze, sepse thjesht nuk shikoja numrin e autobusit. Dhe optometristi im bëri parashikime aspak të mira – problemet kishin filluar me retinën. Dhe XX është yhvilluar për të gjitha problemet me sytë. Çfarë mund të them, ky medikament është i mrekullueshëm! Tani shoh gjithçka shumë më mirë, madje mund të shoh detaje të vogla në distancë. Kështu që unë e rekomandoj me siguri për përdorim! ",
+ 
+"lt64": "Marsela PURIQI,",
+ 
+"lt65": "29 vjeç",
+ 
+"lt66": "Pas lindjes, më filluan problemet me vizionin. Me sa duket, presioni ishte i fortë, dhe vizioni u përkeqësua brenda një kohe mjaft të shkurtër. Më thanë që një gjë e tillë ndodh dhe se së shpejti do të kalonte. Por pas vetëm një viti shikimi u përkeqësua më keq. Kisha dhimbje, skuqje, thatësi. Nga minus 0,5 u shndërrua në minus 2. Unë iu drejtova një klinike private tek një mjek me pagesë, sepse në klinikën shtetërore ata më thanë thjesht se do të kalonte.Një mjek normal në një klinikë private më përshkroi XX, dhe gjithçka u përmirësua brenda vetëm një muaji! Shikimi u rikthyet gradualisht, nuk është më -2 , por  -1, pothuajse 100%. Në lidhje me presionin dhe dhimbjen që ndieja, dhe madje edhe për thatësinë e syve, kam harruar prej kohësh se çfarë janë! ",
+ 
+"lt67": "Gëzim HYMERI",
+ 
+"lt68": "42 vjeç",
+ 
+"lt69": "Unë jam një programues, dhe puna e vazhdueshme para monitorit e dëmtuan shumë shikimin tim. Ajo arriti deri në atë pikë sa ngrihesha nga kompjuteri, dhe gjithçka turbullohej para syve të mi. Unë vuaja nga dhimbje koke të vazhdueshme për shkak të tensionit të rritur në sytë e mi. Reagova vetëm atëhere kur nuk mundja më të punoja para kompjuterit, sytë ndaluan së dalluari shkronja të vogla në ekran. Vendosa të bëja një pushim, porosita XX. U dhashë syve pushim nga kompjuteri për 2 javë, mora preparation në fjalë dhe fillova ta përdor atë menjëherë, gjithashtu fillova të bëj gjimnastikë për 15 minuta në ditë. Rezultati, natyrisht, më mahniti. Unë shoh gjithçka kaq qartë tashmë , sikur të më kishin bërë pastrim sysh! Tani do ta ndjek gjithmonë me kujdes ecurinë e shikimit tim! ",
+ 
+"lt71": "Ktheni shikimin tuaj <span> </span> <i> <mark> me vetëm një kurs </mark> </i>",
+ 
+"lt72": "<span> Eleminon tensionin </span> dhe dhimbjet e syve brenda vetëm 5 minutave",
+ 
+"lt73": "<span> Riktheni shikimin </span> me 99% për 1 kurs përdorimi të rregullt",
+ 
+"lt74": "<span> Parandalon zhvillimin e sëmundjeve të syrit dhe shikimin e dëmtuar",
+ 
+"lt75": "<b> Vepron mbi </b> <span> shkakun </span> e shikimit të dobët",
+ 
+"lt76": "<b> Mbron </b> <span> gjatë ngarkesave </span>të zgjatura",
+ 
+"lt77": " <b>Eleminon</b>tensionin ",
+ 
+"lt78": "Porositeni <span> me zbritje </span> <span> <mark> në këto momente </mark> </span>",
+ 
+"lt81": "Deri në fund të promovimit kanë mbetur:",
+ 
+"lt82": "<div class = \" form__count-item \"> <span> 0 </span> <span> ore </span> </div>: <div class = \"form__count-item \"> <span> 11 </span> <span> minuta </span> </div>: <div class = \"form__count-item \"> <span> 56 </span> <span> sekonda </span> < / div> ",
+ 
+"lt83": "Shteti",
+ 
+"lt84": "Emri juaj",
+ 
+"lt85": "Numri juaj I telefonit",
+ 
+"lt86": "Blini me zbritje",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
